32678d83264980715388e3a363815cf631e66e54
Erik Lopez <maileks@gmail.com>
2021-02-15 11:24:19 +0900

    Conform UIViewController to Presentable

---
+++ SimpleChat/Application/Extensions/UIViewController+Presentable.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+Presentable.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/10.
+//
+
+import UIKit
+
+extension UIViewController: Presentable {
+    func toPresent() -> Self {
+        return self
+    }
+}
