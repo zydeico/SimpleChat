efefb846a7b6cfc8d7e8c9722ebf79d24eb86f60
Erik Lopez <maileks@gmail.com>
2021-02-15 11:20:43 +0900

    Create HomeCoordinator

---
+++ SimpleChat/Application/Scenes/Home/HomeCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  HomeCoordinator.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/09.
+//
+
+import Foundation
+
+final class HomeCoordinator: Coordinator {
+    let router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func start() {
+    }
+}
