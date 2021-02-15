3167787aa6f5c1853ab324fd1fa91e101a63bd87
Erik Lopez <maileks@gmail.com>
2021-02-15 14:02:37 +0900

    Create CoordinatorAllocatorTests

---
+++ SimpleChatTests/Navigation/Coordinator/CoordinatorAllocatorTests.swift
@@ -0,0 +1,12 @@
+//
+//  CoordinatorAllocatorTests.swift
+//  SimpleChatTests
+//
+//  Created by Erik Lopez on 2021/02/15.
+//
+
+import XCTest
+@testable import SimpleChat
+
+final class CoordinatorAllocatorTests: XCTestCase {
+}
