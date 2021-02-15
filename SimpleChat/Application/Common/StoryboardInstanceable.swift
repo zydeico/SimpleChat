a9574d96eae849e501b17c396c100c132c761687
Erik Lopez <maileks@gmail.com>
2021-02-15 11:25:04 +0900

    Create StoryboardInstanceable protocol

---
+++ SimpleChat/Application/Common/StoryboardInstanceable.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardInstanceable.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/10.
+//
+
+import Foundation
+
+protocol StoryboardInstanceable {
+    static var storyboardName: String { get }
+}
