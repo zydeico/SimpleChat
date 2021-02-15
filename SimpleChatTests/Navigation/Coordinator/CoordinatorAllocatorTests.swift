55772f35b89b12fe2173418277870b31a5431955
Erik Lopez <maileks@gmail.com>
2021-02-15 14:03:06 +0900

    Setup CoordinatorAllocatorTests

--- SimpleChatTests/Navigation/Coordinator/CoordinatorAllocatorTests.swift
+++ SimpleChatTests/Navigation/Coordinator/CoordinatorAllocatorTests.swift
@@ -9,4 +9,16 @@ import XCTest
 @testable import SimpleChat
 
 final class CoordinatorAllocatorTests: XCTestCase {
+    let coordinator = DummyCoordinator()
+    var sut: CoordinatorAllocator!
+
+    override func setUp() {
+        super.setUp()
+        sut = CoordinatorAllocator()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+    }
 }
