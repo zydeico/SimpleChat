35a5c6ad9f002b12ca704a25d74328eac16d5044
Erik Lopez <maileks@gmail.com>
2021-02-16 15:44:01 +0900

    Create ChatViewController

---
+++ SimpleChat/Application/Scenes/Chat/ViewControllers/ChatViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChatViewController.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/16.
+//
+
+import UIKit
+
+final class ChatViewController: UIViewController, StoryboardInstanceable {
+    static var storyboardName: String = "Chat"
+}
