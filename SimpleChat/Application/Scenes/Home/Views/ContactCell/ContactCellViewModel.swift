fbeb665cdfff0e16e851b9d0315b235ef266de9c
Erik Lopez <maileks@gmail.com>
2021-02-16 14:05:26 +0900

    Create ContactCellViewModel

---
+++ SimpleChat/Application/Scenes/Home/Views/ContactCell/ContactCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContactCellViewModel.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/16.
+//
+
+import Foundation
+
+struct ContactCellViewModel {
+    let name: String
+    let lastName: String?
+    let imageData: Data?
+}
