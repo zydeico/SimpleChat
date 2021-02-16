567e0035b632d6812c9d2570921a36eb8622d464
Erik Lopez <maileks@gmail.com>
2021-02-16 14:15:56 +0900

    Set up collectionView on HomeViewController

--- SimpleChat/Application/Scenes/Home/ViewControllers/HomeViewController.swift
+++ SimpleChat/Application/Scenes/Home/ViewControllers/HomeViewController.swift
@@ -14,12 +14,25 @@ final class HomeViewController: UIViewController, StoryboardInstanceable {
     
     var viewModel: HomeViewModel!
     
+    private let cellReuseIdentifier = "ContactCollectionViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNavigationBar()
+        setUpCollectionView()
     }
     
     private func setUpNavigationBar() {
         navigationItem.title = viewModel.title
+    }
+    
+    private func setUpCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        let padding: CGFloat = 10.0
+        layout.itemSize = CGSize(width: collectionView.frame.width - padding, height: 100.0)
+        layout.sectionInset = UIEdgeInsets(top: 10.0, left: 0.0, bottom: 10.0, right: 0.0)
+        layout.minimumLineSpacing = 5.0
+        collectionView.collectionViewLayout = layout
+        collectionView.register(UINib(nibName: cellReuseIdentifier, bundle: nil), forCellWithReuseIdentifier: cellReuseIdentifier)
     }
 }
