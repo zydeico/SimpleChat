c01871189fa9b7e3c63076218ed48a2651d2d284
Erik Lopez <maileks@gmail.com>
2021-02-16 14:03:44 +0900

    Set up ContactCollectionViewCell layer

--- SimpleChat/Application/Scenes/Home/Views/ContactCell/ContactCollectionViewCell.swift
+++ SimpleChat/Application/Scenes/Home/Views/ContactCell/ContactCollectionViewCell.swift
@@ -12,4 +12,16 @@ final class ContactCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var lastNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpLayer()
+    }
+    
+    private func setUpLayer() {
+        layer.shadowColor = UIColor(named: "Shadow")?.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 2.0
+    }
 }
