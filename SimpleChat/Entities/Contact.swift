3fa920dd736e751bb4f5757c84ed84474fdcec27
Erik Lopez <maileks@gmail.com>
2021-02-17 11:58:42 +0900

    Create Contact entity

---
+++ SimpleChat/Entities/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/17.
+//
+
+import Foundation
+
+struct Contact {
+    let name: String
+    let lastName: String
+    let imageData: Data?
+}
