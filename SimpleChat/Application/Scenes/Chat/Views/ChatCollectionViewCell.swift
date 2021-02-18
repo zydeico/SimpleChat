3bfda7b7578534c1129faf82edffcc68be57d651
Erik Lopez <maileks@gmail.com>
2021-02-18 15:35:10 +0900

    Create ChatCollectionViewCell

---
+++ SimpleChat/Application/Scenes/Chat/Views/ChatCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatCollectionViewCell.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/17.
+//
+
+import UIKit
+
+final class ChatCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var mainView: UIView!
+    @IBOutlet private weak var label: UILabel!
+}
