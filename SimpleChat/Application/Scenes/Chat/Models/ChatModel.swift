a27a4c8162ee4297e67455d0faa309170dd3efe0
Erik Lopez <maileks@gmail.com>
2021-02-18 16:41:47 +0900

    Create ChatModel

---
+++ SimpleChat/Application/Scenes/Chat/Models/ChatModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChatModel.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/18.
+//
+
+import Foundation
+
+struct ChatModel {
+    let message: String
+    let isSentByUser: Bool
+}
