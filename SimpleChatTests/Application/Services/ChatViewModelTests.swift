3343d9fe8f48ac8d46efb56f69e05546ae2d67b6
Erik Lopez <maileks@gmail.com>
2021-02-20 14:17:19 +0900

    Initialize ChatViewModel tests

--- SimpleChatTests/Application/Services/ChatViewModelTests.swift
+++ SimpleChatTests/Application/Services/ChatViewModelTests.swift
@@ -10,4 +10,23 @@ import XCTest
 @testable import SimpleChat
 
 final class ChatViewModelTests: XCTestCase {
+    let contact = Contact(name: "Name", lastName: "LastName", imageData: nil)
+    var autoResponder: MockAutoResponseProvider!
+    var sut: ChatViewModel!
+    var delegate: MockChatViewModelDelegate!
+    
+    override func setUp() {
+        super.setUp()
+        autoResponder = MockAutoResponseProvider()
+        delegate = MockChatViewModelDelegate()
+        sut = ChatViewModel(contact: contact, autoResponder: autoResponder)
+        sut.delegate = delegate
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        autoResponder = nil
+        delegate = nil
+    }
 }
