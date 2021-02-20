ac03308d18a3061e4c0e01acae3dd51d856a39d6
Erik Lopez <maileks@gmail.com>
2021-02-20 14:17:00 +0900

    Create ChatViewModelTests

---
+++ SimpleChatTests/Application/Services/ChatViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  ChatViewModelTests.swift
+//  SimpleChatTests
+//
+//  Created by Erik Lopez on 2021/02/20.
+//
+//  swiftlint:disable weak_delegate
+
+import XCTest
+@testable import SimpleChat
+
+final class ChatViewModelTests: XCTestCase {
+}
