8188d0bc83b4700c72c8e9b9948c848ae63bf6eb
Erik Lopez <maileks@gmail.com>
2021-02-23 19:22:11 +0900

    Set up title label

--- SimpleChat/Application/Scenes/Home/Views/BackgroundView/BackgroundView.swift
+++ SimpleChat/Application/Scenes/Home/Views/BackgroundView/BackgroundView.swift
@@ -10,4 +10,15 @@ import UIKit
 final class BackgroundView: UIView {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var subtitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpTitleLabel()
+    }
+    
+    private func setUpTitleLabel() {
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+    }
 }
