2f62f5294f91c8165b93dcfed9a73e140940ba06
Erik Lopez <maileks@gmail.com>
2021-02-23 19:21:43 +0900

    Create BackgroundView

---
+++ SimpleChat/Application/Scenes/Home/Views/BackgroundView/BackgroundView.swift
@@ -0,0 +1,13 @@
+//
+//  BackgroundView.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/23.
+//
+
+import UIKit
+
+final class BackgroundView: UIView {
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subtitleLabel: UILabel!
+}
