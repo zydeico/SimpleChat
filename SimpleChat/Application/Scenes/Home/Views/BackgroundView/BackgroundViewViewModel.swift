52682e9527697217e71de9d084df996ece44d836
Erik Lopez <maileks@gmail.com>
2021-02-23 19:22:49 +0900

    Create BackgroundViewViewModel

---
+++ SimpleChat/Application/Scenes/Home/Views/BackgroundView/BackgroundViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BackgroundViewViewModel.swift
+//  SimpleChat
+//
+//  Created by Erik Lopez on 2021/02/23.
+//
+
+import Foundation
+
+struct BackgroundViewViewModel: Equatable {
+    let title: String
+    let subtitle: String
+}
